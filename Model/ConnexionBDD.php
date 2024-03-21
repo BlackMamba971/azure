5b7e361fbf881b492f4bc18fcfee444af637ef6e
JEAN-GILLES Kyllian <92990556+BlackMamba971@users.noreply.github.com>
2024-03-21 22:10:17 +0100

    Update ConnexionBDD.php

--- Model/ConnexionBDD.php
+++ Model/ConnexionBDD.php
@@ -10,14 +10,14 @@ class Conn
 {
     private static $instance = null;
     private $connexion;
-    private static $host = 'gestionnaire-des-apprentis.database.windows.net';
-    private static $dbname = 'gestionnaire-des-apprentis.azurewebsites.net';
-    private static $username = 'YcK7896aGsSgk2';
-    private static $password = 'aScG7Y698s2kgK';
+    private static $host = 'saeserv.postgres.database.azure.com';
+    private static $dbname = 'postgres';
+    private static $username = 'nwaarr';
+    private static $password = 'Kyll!Mario644';
 
     private function __construct()
     {
-        $dsn = "pgsql:host=" . self::$host . ";port=1433;dbname=" . self::$dbname . ";user=" . self::$username . ";password=" . self::$password;
+        $dsn = "sqlsrv::host=" . self::$host . ";port=5432;dbname=" . self::$dbname . ";user=" . self::$username . ";password=" . self::$password;
 
         try {
             $this->connexion = new PDO($dsn);
