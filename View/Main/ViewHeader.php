edf4375ac34bd129cb173623fe1dda4875f57e12
JEAN-GILLES Kyllian <92990556+BlackMamba971@users.noreply.github.com>
2024-04-18 01:12:28 +0200

    Update ViewHeader.php

--- View/Main/ViewHeader.php
+++ View/Main/ViewHeader.php
@@ -56,7 +56,7 @@ $root ='../';
                         <div class="dropdown">
                             <button type="button" onclick="window.location.href ='../Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="entreprise" value="Entreprise" class="btnCreation">Entreprise</button>
                             <div class="dropdown-content">
-                                <button type="button" onclick="window.location.href ='Entreprise/ViewAjoutEntreprise.php'" name="entreprise" value="Entreprise" class="item">Créer une entreprise</button>
+                                <button type="button" onclick="window.location.href ='../Entreprise/ViewAjoutEntreprise.php'" name="entreprise" value="Entreprise" class="item">Créer une entreprise</button>
                                 <button type="button" onclick="window.location.href ='/<?php echo $root ?>Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="entreprise" value="Entreprise" class="item">Rechercher une entreprise</button>
 
                                 <button type="button" onclick="window.location.href ='/<?php echo $root ?>Offre/ViewAjoutOffre.php'" name="Offre" value="Offre" class="item">Créer une offre</button>
@@ -69,10 +69,10 @@ $root ='../';
                         echo ('
                         <li>
                             <div class="dropdown">
-                                <button type="button" onclick="window.location.href =\'/' . $root . 'Personnel/ViewAdminAdministration.php\'" name="administration" value="administration" class="btnCreation">Administration</button>
+                                <button type="button" onclick="window.location.href =\'/' . $root . '../Personnel/ViewAdminAdministration.php\'" name="administration" value="administration" class="btnCreation">Administration</button>
                                 <div class="dropdown-content">
-                                    <button type="button" onclick="window.location.href =\'/' . $root . 'Personnel/ViewAjoutAdministration.php\'" name="administration" value="administration" class="item">Créer un membre du personnel</button>
-                                    <button type="button" onclick="window.location.href =\'/' . $root . 'Personnel/ViewAdminAdministration.php\'" name="administration" value="administration" class="item">Voir les membres du personnel</button>
+                                    <button type="button" onclick="window.location.href =\'/' . $root . '../Personnel/ViewAjoutAdministration.php\'" name="administration" value="administration" class="item">Créer un membre du personnel</button>
+                                    <button type="button" onclick="window.location.href =\'/' . $root . '../Personnel/ViewAdminAdministration.php\'" name="administration" value="administration" class="item">Voir les membres du personnel</button>
                                 </div>
                             </div>
                         </li>
@@ -85,9 +85,9 @@ $root ='../';
                         <a href="/<?php echo $root ?>/english/View/ViewAdminMainEn.php"> <img src="/<?php echo $root ?>/asserts/img/traduction.png" alt="Icone de traduction" class="traduction" id="trad"></a>
                     </li>
                     <li id="account-photo">
-                        <img id="photo" src="/<?php echo $root ?>/asserts/img/utilisateur.png" alt="Image de l'utilisateur" class="utilisateur">
+                        <img id="photo" src="/<?php echo $root ?>../asserts/img/utilisateur.png" alt="Image de l'utilisateur" class="utilisateur">
                         <div id="account-dropdown">
-                            <form method="post" action="/<?php echo $root ?>/Controller/Connexion/ControllerBtnDeco.php">
+                            <form method="post" action="/<?php echo $root ?>../Controller/Connexion/ControllerBtnDeco.php">
                                 <input class="" name="compte" type="submit" value="Mon compte">
                                 <input class="" name="deco" type="submit" value="Se déconnecter">
                             </form>
@@ -109,16 +109,16 @@ $root ='../';
 
 
     <div class="menu-container2">
-        <form method="post" action="/<?php echo $root ?>/Controller/Connexion/ControllerBtnDeco.php">
+        <form method="post" action="/<?php echo $root ?>.../Controller/Connexion/ControllerBtnDeco.php">
             <ul class="vertical-menu-burger">
                 <li>
                     <a href="/<?php echo $root ?>/english/View/ViewAdminMainEn.php"> <img src="/<?php echo $root ?>/asserts/img/traduction.png" alt="Icone de traduction" class="traduction" id="trad"></a>
                 </li>
 
                 <li id="account-photo2">
-                    <img id="photo2" src="/<?php echo $root ?>/asserts/img/utilisateur.png" alt="Image de l'utilisateur" class="utilisateur">
+                    <img id="photo2" src="/<?php echo $root ?>../asserts/img/utilisateur.png" alt="Image de l'utilisateur" class="utilisateur">
                     <div id="account-dropdown2">
-                        <form method="post" action="/Controller/Connexion/ControllerBtnDeco.php">
+                        <form method="post" action="../Controller/Connexion/ControllerBtnDeco.php">
                             <input class="" name="compte" type="submit" value="Mon compte">
                             <input class="" name="deco" type="submit" value="Se déconnecter">
                         </form>
@@ -141,7 +141,7 @@ $root ='../';
             <div class="menu-icon">&#9776;</div>
             <ul class="menu">
                 <li>
-                    <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Main/View<?php echo $_SESSION['role'] ?>Main.php'" name="accueil" value="Accueil" class="btnCreation">  Accueil </button>
+                    <button type="button" onclick="window.location.href ='View/Main/View<?php echo $_SESSION['role'] ?>Main.php'" name="accueil" value="Accueil" class="btnCreation">  Accueil </button>
                 </li>
                 <li>
                     <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Etudiant/View<?php echo $_SESSION['role'] ?>Etu.php'" name="etudiant" value="Etudiant" class="btnCreation">Etudiant</button>
@@ -149,11 +149,11 @@ $root ='../';
                     <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Etudiant/View<?php echo $_SESSION['role'] ?>Etu.php'" name="etudiant" value="Etudiant" class="sousBtnCreation">Rechercher un étudiant</button>
                 </li>
                 <li>
-                    <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="entreprise" value="Entreprise" class="btnCreation">Entreprise</button>
-                    <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Entreprise/ViewAjoutEntreprise.php'" name="entreprise" value="Entreprise" class="sousBtnCreation">Créer une entreprise</button>
-                    <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="entreprise" value="Entreprise" class="sousBtnCreation">Rechercher une entreprise</button>
-                    <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Offre/ViewAjoutOffre.php'" name="Offre" value="Offre" class="sousBtnCreation">Créer une offre</button>
-                    <button type="button" onclick="window.location.href ='/<?php echo $root ?>/View/Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="Offre" value="Offre" class="sousBtnCreation">Rechercher une offre</button>
+                    <button type="button" onclick="window.location.href ='/View/Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="entreprise" value="Entreprise" class="btnCreation">Entreprise</button>
+                    <button type="button" onclick="window.location.href ='/View/Entreprise/ViewAjoutEntreprise.php'" name="entreprise" value="Entreprise" class="sousBtnCreation">Créer une entreprise</button>
+                    <button type="button" onclick="window.location.href ='/View/Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="entreprise" value="Entreprise" class="sousBtnCreation">Rechercher une entreprise</button>
+                    <button type="button" onclick="window.location.href ='/View/Offre/ViewAjoutOffre.php'" name="Offre" value="Offre" class="sousBtnCreation">Créer une offre</button>
+                    <button type="button" onclick="window.location.href ='/View/Entreprise/View<?php echo $_SESSION['role'] ?>Entreprise.php'" name="Offre" value="Offre" class="sousBtnCreation">Rechercher une offre</button>
 
                 </li>
                 <?php
@@ -171,11 +171,11 @@ $root ='../';
         </div>
     </div>
 
-    <script src="/<?php echo $root ?>/asserts/js/script.js" defer></script>
-    <script src="/<?php echo $root ?>/asserts/js/header.js" defer></script>
-    <link rel="icon" href="/<?php echo $root ?>/asserts/img/logo.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="/<?php echo $root ?>/asserts/css/Cloche.css">
-    <script src="/<?php echo $root ?>/asserts/js/script.js"></script>
+    <script src="/<?php echo $root ?>../asserts/js/script.js" defer></script>
+    <script src="/<?php echo $root ?>../asserts/js/header.js" defer></script>
+    <link rel="icon" href="/<?php echo $root ?>../asserts/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="/<?php echo $root ?>../asserts/css/Cloche.css">
+    <script src="/<?php echo $root ?>../asserts/js/script.js"></script>
 
 
 </header>
